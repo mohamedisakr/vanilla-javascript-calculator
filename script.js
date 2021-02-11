3ac78c800630f0bbacbdff5d8a542258b5ce6c2c
mohamedisakr <mohamedisakr@gmail.com>
2021-02-11 13:12:19 +0200

    implementing clear and backspace functionality

--- script.js
+++ script.js
@@ -1,3 +1,76 @@
+const equalsButton = document.querySelector("#equals");
+const zeroButton = document.querySelector("#zero");
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const addButton = document.querySelector("#add");
+const subtractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
+const decimalButton = document.querySelector("#decimal");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const displayButton = document.querySelector("#display");
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+let result = 0;
+let inputs = [];
+
+function init() {
+  displayButton.textContent = 0;
+  inputs = [];
+  result = 0;
+}
+
+function typeOnDisplay(e) {
+  inputs.push(e.target.textContent);
+  displayButton.textContent = inputs.join("");
+}
+
+init();
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", typeOnDisplay);
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    inputs.push(e.target.textContent);
+  });
+});
+
+clearButton.addEventListener("click", (e) => {
+  init();
+});
+
+deleteButton.addEventListener("click", (e) => {
+  inputs.pop();
+  inputs.length === 0
+    ? (displayButton.textContent = 0)
+    : (displayButton.textContent = inputs.join(""));
+});
+
+// Cannot divide by zero
+equalsButton.addEventListener("click", (e) => {
+  try {
+    result = eval(buffer);
+  } catch (error) {
+    // alert(error);
+  }
+
+  displayButton.textContent = result;
+  // buffer = ``;
+  inputs = [];
+});
+
 /*
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -125,4 +198,32 @@ deleteButton.addEventListener('click', button => {
   calculator.delete()
   calculator.updateDisplay()
 })
+*/
+
+/*
+const controlIds = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "add",
+  "subtract",
+  "multiply",
+  "divide",
+  "decimal",
+  "clear",
+  "display",
+];
+
+let genCode = ``;
+controlIds.forEach((control) => {
+  genCode += `const ${control}Button = document.querySelector("#${control}");\n`;
+});
+console.log(genCode);
 */
